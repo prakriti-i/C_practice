a315ae1845230442be311b5353dd44ebe923019f
Prakriti <subediprakriti11@gmail.com>
2025-01-27 15:32:01 +0545

    update

---
+++ aaaa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int i, p;
+    for(i=1; i<=11; i++)
+    {
+        p=pow(3,i);
+        printf("%d \t",p);
+    }
+    return 0;
+    
+}
